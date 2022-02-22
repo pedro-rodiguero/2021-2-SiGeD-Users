95b2ac50c0568983458bf44c541aeedda3cbb441
murilo-dan <murilo.loiola.dan@gmail.com>
2022-02-22 18:10:54 -0300

    Testing metrics extraction

--- scripts/consts.js
+++ scripts/consts.js
@@ -1,4 +1,4 @@
-const REPO = '2020-2-SiGeD-Users';
+const REPO = '2021-2-SiGeD-Users';
 const OWNER = 'fga-eps-mds';
 const SONAR_ID = 'fga-eps-mds_2020-2-G4-Users';
 const SONAR_URL = `https://sonarcloud.io/api/measures/component_tree?component=${SONAR_ID}&metricKeys=files,functions,complexity,coverage,ncloc,comment_lines_density,duplicated_lines_density,security_rating,tests,test_success_density,test_execution_time,reliability_rating&ps=500`;
