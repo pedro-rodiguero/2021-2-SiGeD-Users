9ba60b0202fad2491aa93654784cac231c9b3fb0
murilo-dan <murilo.loiola.dan@gmail.com>
2022-02-26 17:11:55 -0300

    Testing deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const {
   DB_PASS,
   DB_NAME,
   DB_HOST,
-  PORT,
 } = process.env;
 
 const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`;
@@ -27,7 +26,8 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
