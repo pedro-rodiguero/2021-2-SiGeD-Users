b435de6155b6c71a22e9441b8ed7484d3fc2770f
murilo-dan <murilo.loiola.dan@gmail.com>
2022-02-26 16:42:49 -0300

    Testing deploy

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(routes);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
